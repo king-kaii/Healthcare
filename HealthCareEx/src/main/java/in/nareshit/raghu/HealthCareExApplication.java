cafdf6ae2670195cc787fd9a21e5be289ce22ed5
king-kaii <kaibalyanahak0703@gmail.com>
2023-08-04 22:29:11 +0530

    First Commit From RRR

---
+++ HealthCareEx/src/main/java/in/nareshit/raghu/HealthCareExApplication.java
@@ -0,0 +1,14 @@
+package in.nareshit.raghu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareExApplication.class, args);
+		System.out.println("Welcome to Health care Project");
+	}
+
+}
